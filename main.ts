ce92e7ff58b92c68bbdaef447cb997031d165579
gijslammers <100514114+gijslammers@users.noreply.github.com>
2022-02-27 12:20:05 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 basic.forever(function () {
-    if (input.compassHeading() > 315 && input.compassHeading() < 45) {
+    if (input.compassHeading() > 338 && input.compassHeading() < 23) {
         basic.showLeds(`
             . . # . .
             . . # . .
@@ -7,15 +7,61 @@ basic.forever(function () {
             . # # # .
             # # # # #
             `)
+    } else if (input.compassHeading() > 22 && input.compassHeading() < 68) {
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            # # # . .
+            # # # . .
+            # # # . .
+            `)
+    } else if (input.compassHeading() > 67 && input.compassHeading() < 113) {
+        basic.showLeds(`
+            # . . . .
+            # # . . .
+            # # # # #
+            # # . . .
+            # . . . .
+            `)
+    } else if (input.compassHeading() > 112 && input.compassHeading() < 158) {
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            # # # . .
+            . . . # .
+            . . . . #
+            `)
+    } else if (input.compassHeading() > 157 && input.compassHeading() < 203) {
+        basic.showLeds(`
+            # # # # #
+            . # # # .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    } else if (input.compassHeading() > 202 && input.compassHeading() < 248) {
+        basic.showLeds(`
+            . . # # #
+            . . # # #
+            . . # # #
+            . # . . .
+            # . . . .
+            `)
+    } else if (input.compassHeading() > 247 && input.compassHeading() < 293) {
+        basic.showLeds(`
+            . . . . #
+            . . . # #
+            # # # # #
+            . . . # #
+            . . . . #
+            `)
     } else {
-        if (input.compassHeading() > 340) {
-            basic.showLeds(`
-                . # # # .
-                # # # # #
-                # # . # #
-                # # # # #
-                # # # # #
-                `)
-        }
+        basic.showLeds(`
+            # . . . .
+            . # . . .
+            . . # # #
+            . . # # #
+            . . # # #
+            `)
     }
 })
